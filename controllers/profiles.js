3bae259e82c42978b1bb75edfbb518f2fb7d7a86
SajedaHussain <sajedataqi22@gmail.com>
2025-12-15 10:31:37 +0300

    finished the profile controller and model

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,27 +2,28 @@ const express = require('express')
 const router = express.Router()
 const Profile = require('../models/profile')
 
-// SHOW profile (by profile ID)
-router.get('/:id', async (req, res) => {
+// NEW Profile =======================================================================
+router.get('/new', (req, res) => {
+  res.render('profiles/new.ejs')
+})
+
+// CREATE Profile=====================================================================
+router.post('/', async (req, res) => {
   try {
-    const profile = await Profile.findById(req.params.id)
-    res.render('profiles/show.ejs', {
-      profile,
-      user: req.session.user
-    })
+    req.body.owner = req.session.user._id
+    const profile = await Profile.create(req.body)
+    res.redirect(`/profiles/${profile._id}`)
   } catch (err) {
     console.log(err)
     res.redirect('/')
   }
 })
-
-// SHOW profile by USER ID (for job owner)
+ 
+// SHOW profile by USER ID ========================================================== 
 router.get('/user/:userId', async (req, res) => {
   try {
     const profile = await Profile.findOne({ owner: req.params.userId })
-
     if (!profile) return res.redirect('/')
-
     res.render('profiles/show.ejs', {
       profile,
       user: req.session.user
@@ -33,15 +34,28 @@ router.get('/user/:userId', async (req, res) => {
   }
 })
 
-// EDIT
+// SHOW profile by PROFILE ID ==========================================================
+router.get('/:id', async (req, res) => {
+  try {
+    const profile = await Profile.findById(req.params.id)
+    if (!profile) return res.redirect('/')
+    res.render('profiles/show.ejs', {
+      profile,
+      user: req.session.user
+    })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+// EDIT profile ==========================================================
 router.get('/:id/edit', async (req, res) => {
   try {
     const profile = await Profile.findById(req.params.id)
-
     if (!profile.owner.equals(req.session.user._id)) {
       return res.redirect('/')
     }
-
     res.render('profiles/edit.ejs', { profile })
   } catch (err) {
     console.log(err)
@@ -49,15 +63,14 @@ router.get('/:id/edit', async (req, res) => {
   }
 })
 
-// UPDATE
+
+// UPDATE profile ================================================================================
 router.put('/:id', async (req, res) => {
   try {
     const profile = await Profile.findById(req.params.id)
-
     if (!profile.owner.equals(req.session.user._id)) {
       return res.redirect('/')
     }
-
     await Profile.findByIdAndUpdate(req.params.id, req.body)
     res.redirect(`/profiles/${profile._id}`)
   } catch (err) {
