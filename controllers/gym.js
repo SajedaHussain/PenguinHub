05a4cfeaed60646793b54c74719e0d43a936bbe2
SajedaHussain <sajedataqi22@gmail.com>
2025-12-17 09:04:35 +0300

    gy controller done

---
+++ controllers/gym.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const router = express.Router()
+const Profile = require('../models/profile')
+
+router.get('/', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ owner: req.session.user._id })
+
+    if (!profile) return res.redirect('/profiles/new')
+
+    const height = profile.height / 100 // cm → m
+    const weight = profile.weight
+
+    const bmi = (weight / (height * height)).toFixed(1)
+
+    let category = ''
+    let planImage = ''
+
+    if (bmi < 18.5) {
+      category = 'Underweight Penguin'
+      planImage = '/Assest/plans/underweight.png'
+    } else if (bmi < 25) {
+      category = 'Fit Penguin'
+      planImage = '/Assest/plans/fit.png'
+    } else {
+      category = 'Heavy Penguin'
+      planImage = '/Assest/plans/heavy.png'
+    }
+
+    res.render('gym/show.ejs', {
+      profile,
+      bmi,
+      category,
+      planImage
+    })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+module.exports = router
